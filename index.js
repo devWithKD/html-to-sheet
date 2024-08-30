20114d67b8ddb532c9af325c81f59fe80eb21afd
Kedar Joshi <kedarjo2002@gmail.com>
2024-08-30 09:42:22 +0530

    Finale

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import yargs from "yargs";
 const { argv } = yargs(process.argv);
 
 const html = fs.readFileSync(argv.html, "utf8");
+// const html = fs.readFileSync("./data.html", "utf8");
 
 const dom = new JSDOM(html);
 const document = dom.window.document;
@@ -20,13 +21,30 @@ for (let i = 0; i < 5; i++) {
 
   if (table) {
     const worksheet = xlsx.utils.table_to_sheet(table, {
-      cellDates: true,
+      // cellDates: true,
       // UTC: true,
-      // raw: true,
+      raw: true,
     });
     xlsx.utils.book_append_sheet(workbook, worksheet, sheetname);
   }
   // console.log("added sheet");
 }
 
+// for (let i = 0; i < workbook.SheetNames.length; i++) {
+//   const sheet = workbook.Sheets[workbook.SheetNames[i]];
+//   const range = xlsx.utils.decode_range(sheet["!ref"]);
+//   for (let row = range.s.r + 1; row <= range.e.r; ++row) {
+//     for (let column = range.s.c; column <= range.e.c; ++column) {
+//       const ref = xlsx.utils.encode_cell({ r: row, c: column });
+//       const cell = sheet[ref];
+//       if (cell.t == "d") cell.t = "s";
+//     }
+//   }
+// }
+
+// const data = JSON.stringify(xlsx.utils.sheet_to_json(sheet));
+
+// fs.writeFileSync(argv.file, data);
+
 xlsx.writeFile(workbook, argv.spreadsheet);
+// xlsx.writeFile(workbook, "./output.xlsx");
